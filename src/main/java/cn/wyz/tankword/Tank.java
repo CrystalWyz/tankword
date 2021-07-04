e9f6f6a2f6b2442ee7672854b75045514f24c512
wangnanxiang <806543499@qq.com>
2021-07-04 09:54:51 +0800

    边界检测

--- src/main/java/cn/wyz/tankword/Tank.java
+++ src/main/java/cn/wyz/tankword/Tank.java
@@ -122,6 +122,23 @@ public class Tank {
             this.fire();
             this.randomDir();
         }
+
+        boundsCheck();
+    }
+
+    private void boundsCheck() {
+        if(this.x < 2) {
+            this.x = 2;
+        }
+        if(this.y < 32) {
+            this.y = 32;
+        }
+        if(this.x > TankFrame.GAME_WIDTH - Tank.WIDTH - 2) {
+            this.x = TankFrame.GAME_WIDTH - Tank.WIDTH - 2;
+        }
+        if(this.y > TankFrame.GAME_HEIGHT - Tank.HEIGHT - 2) {
+            this.y = TankFrame.GAME_HEIGHT - Tank.HEIGHT - 2;
+        }
     }
 
     private void randomDir() {
