df444e26fb7c91d52c5fda4ba4daff71eb11189f
wangnanxiang <806543499@qq.com>
2021-07-04 09:36:13 +0800

    敌我坦克区分

--- src/main/java/cn/wyz/tankword/Tank.java
+++ src/main/java/cn/wyz/tankword/Tank.java
@@ -1,6 +1,7 @@
 package cn.wyz.tankword;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.util.Random;
 
 /**
@@ -10,8 +11,8 @@ public class Tank {
     private int x, y;
     private Dir dir;
     private static final int SPEED = 3;
-    public static final int WIDTH = ResourceMgr.tankU.getWidth();
-    public static final int HEIGHT = ResourceMgr.tankU.getHeight();
+    public static final int WIDTH = ResourceMgr.goodTankU.getWidth();
+    public static final int HEIGHT = ResourceMgr.goodTankU.getHeight();
     private Group group = Group.BAD;
 
     public boolean live = true;
@@ -28,6 +29,9 @@ public class Tank {
         this.dir = dir;
         this.group = group;
         this.tankFrame = tankFrame;
+        if(group == Group.GOOD) {
+            moving = false;
+        }
     }
 
     public int getX() {
@@ -79,16 +83,16 @@ public class Tank {
 
         switch (dir) {
             case UP:
-                g.drawImage(ResourceMgr.tankU, x, y, null);
+                g.drawImage(this.group == Group.GOOD ? ResourceMgr.goodTankU : ResourceMgr.badTankU, x, y, null);
                 break;
             case DOWN:
-                g.drawImage(ResourceMgr.tankD, x, y, null);
+                g.drawImage(this.group == Group.GOOD ? ResourceMgr.goodTankD : ResourceMgr.badTankD, x, y, null);
                 break;
             case LEFT:
-                g.drawImage(ResourceMgr.tankL, x, y, null);
+                g.drawImage(this.group == Group.GOOD ? ResourceMgr.goodTankL : ResourceMgr.badTankL, x, y, null);
                 break;
             case RIGHT:
-                g.drawImage(ResourceMgr.tankR, x, y, null);
+                g.drawImage(this.group == Group.GOOD ? ResourceMgr.goodTankR : ResourceMgr.badTankR, x, y, null);
                 break;
             default:break;
         }
@@ -125,8 +129,8 @@ public class Tank {
     }
 
     public void fire() {
-        int bX = x + ResourceMgr.tankU.getWidth() / 2 - ResourceMgr.bulletU.getWidth() / 2;
-        int bY = y + ResourceMgr.tankU.getHeight() / 2 - ResourceMgr.bulletU.getHeight() / 2;
+        int bX = x + ResourceMgr.goodTankU.getWidth() / 2 - ResourceMgr.bulletU.getWidth() / 2;
+        int bY = y + ResourceMgr.goodTankU.getHeight() / 2 - ResourceMgr.bulletU.getHeight() / 2;
         tankFrame.bulletList.add(new Bullet(bX, bY, dir, this.group, tankFrame));
     }
 
